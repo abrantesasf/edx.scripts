4a717601e7de9b9ba6649de169461b2e6ecf2f2d
Abrantes Araújo Silva Filho <abrantesasf@gmail.com>
2019-01-05 18:43:10 +0000

    Criação de script para verificar o status do Open edX

---
+++ edx.platform-status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#---------------------------------------------------------
+# Por: Abrantes Araújo Silva Filho
+#      http://abrantesfilho.com
+#      abrantesasf@gmail.com
+#---------------------------------------------------------
+
+# Status do Open edX:
+sudo /edx/bin/supervisorctl status
+sudo service nginx status
